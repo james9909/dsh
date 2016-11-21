b54f94393ac9a51f3c018f1181935cef39406bc3
Nobel <nobel.gautam@gmail.com>
2016-11-21 11:31:23 -0500

    initial version

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+    char buf[512] = {};
+    char *argl[512] = {};
+    char *args = buf;
+    char *arg;
+    printf("%% ");
+    while (fgets(buf, sizeof(buf), stdin))
+    {
+        args = buf;
+        buf[strlen(buf)-1] = 0;
+        int i = 0;
+        for (i = 0; args; ++i)
+        {
+            arg = strsep(&args, " ");
+            argl[i] = arg;
+        }
+        argl[i] = 0;
+
+        pid_t n = fork();
+        if (n == 0)
+        {
+            execvp(argl[0], argl);
+        }
+        int status;
+        wait(&status);
+        printf("%% ");
+    }
+    return 0;
+}
