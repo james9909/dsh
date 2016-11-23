f96ecb2044cdf3ffc731180e861a0242b565ad54
James Wang <jameswang9909@hotmail.com>
2016-11-22 20:13:38 -0500

    Fix colors from causing overflow issues
    https://stackoverflow.com/questions/9468435/look-how-to-fix-column-calculation-in-python-readline-if-use-color-prompt/9468954#9468954

--- colors.h
+++ colors.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define BLUE "\x1b[34m"
-#define GREEN "\x1b[32m"
-#define MAGENTA "\x1b[35m"
-#define RED "\x1b[31m"
-#define RESET "\x1b[0m"
-#define YELLOW "\x1b[33m"
+#define BLUE "\001\x1b[34m\002"
+#define GREEN "\001\x1b[32m\002"
+#define MAGENTA "\001\x1b[35m\002"
+#define RED "\001\x1b[31m\002"
+#define RESET "\001\x1b[0m\002"
+#define YELLOW "\001\x1b[33m\002"
