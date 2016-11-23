7b82448b8899522afd3368dd1f7cc153a9b25c18
ThisIsMyNick <ThisIsMyNick@users.noreply.github.com>
2016-11-22 20:49:28 -0500

    zero everything just in case

--- executor.c
+++ executor.c
@@ -132,8 +132,8 @@ void handle_redirect(char *argv[512])
 
 void handle_pipes(char *cmd, int num_pipes)
 {
-    char *pipe_cmds[512];
-    int pfds[2*num_pipes];
+    char *pipe_cmds[512] = {};
+    int pfds[2*num_pipes] = {};
     int i = 0;
     for (i = 0; i < num_pipes; ++i)
     {
@@ -189,7 +189,7 @@ void handle_pipes(char *cmd, int num_pipes)
 
 void run(char *input)
 {
-    char *cmds[512];
+    char *cmds[512] = {};
     char *argv[512] = {};
     int i,j;
     for (i = 0; cmds[i] = strsep(&input, ";"); ++i)
