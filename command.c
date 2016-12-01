32b8913a668960de40a7b63f6b87cf9435f9af80
James Wang <jameswang9909@hotmail.com>
2016-11-30 22:33:57 -0500

    Fix boolean conversions

--- command.c
+++ command.c
@@ -9,7 +9,7 @@
 
 void handle_redirects(Command *c)
 {
-    if (c->stdin_redir_f)
+    if (*c->stdin_redir_f)
     {
         int fd = open(c->stdin_redir_f, O_RDONLY);
         dup2(fd, STDIN_FILENO);
@@ -20,7 +20,7 @@ void handle_redirects(Command *c)
         dup2(c->stdout_redir, STDOUT_FILENO);
         close(c->stdout_redir);
     }
-    if (c->stdout_redir_f)
+    if (*c->stdout_redir_f)
     {
         int append = c->stdout_append ? O_APPEND : 0;
         int fd = open(c->stdout_redir_f, O_WRONLY|O_CREAT|append, 0644);
@@ -32,7 +32,7 @@ void handle_redirects(Command *c)
         dup2(c->stderr_redir, STDERR_FILENO);
         close(c->stderr_redir);
     }
-    if (c->stderr_redir_f)
+    if (*c->stderr_redir_f)
     {
         int append = c->stderr_append ? O_APPEND : 0;
         int fd = open(c->stderr_redir_f, O_WRONLY|O_CREAT|append, 0644);
