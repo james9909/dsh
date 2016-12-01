1cce150eb113d64978771d19e801009eafc6be09
James Wang <jameswang9909@hotmail.com>
2016-11-30 23:29:30 -0500

    Remove old code

--- aliases.c
+++ aliases.c
@@ -35,22 +35,6 @@ void add_alias(char *alias, char* replacement)
     }
 }
 
-/*
-void handle_aliases(char *argv[512])
-{
-    int i;
-    for (i = 0; argv[i]; ++i)
-    {
-        int index = 0;
-        if ((index = find_alias(argv[i])) != -1)
-        {
-            free(argv[i]);
-            argv[i] = split(aliases[1][index]);
-        }
-    }
-}
-*/
-
 void handle_aliases(Command *c)
 {
     int i;
