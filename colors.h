9f8997dbaeb11c75fe6fed12d69d63eb0e406438
James Wang <jameswang9909@hotmail.com>
2016-12-01 11:27:29 -0500

    Use 003 instead of x1b for colors

--- colors.h
+++ colors.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define BLUE "\001\x1b[34m\002"
-#define GREEN "\001\x1b[32m\002"
-#define MAGENTA "\001\x1b[35m\002"
-#define RED "\001\x1b[31m\002"
-#define RESET "\001\x1b[0m\002"
-#define YELLOW "\001\x1b[33m\002"
+#define BLUE "\001\033[34m\002"
+#define GREEN "\001\033[32m\002"
+#define MAGENTA "\001\033[35m\002"
+#define RED "\001\033[31m\002"
+#define RESET "\001\033[0m\002"
+#define YELLOW "\001\033[33m\002"
